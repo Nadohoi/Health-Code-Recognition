547f90356d3d22c99a4a91a0bda8c572f51dcab8
Nadohoi <nadohoi0403@petalmail.com>
2025-08-31 11:54:16 +0800

    Sound notification added

--- ver.4/recognition_v4.py
+++ ver.4/recognition_v4.py
@@ -1,7 +1,10 @@
 # Import the necessary packages
 from imutils.perspective import four_point_transform
-import imutils, cv2, os, sys
+from imutils.video import WebcamVideoStream
+import cv2, os, sys
 import numpy as np
+import winsound
+from time import sleep
 
 def classify():
     # returns a 2D array, where each pixel is either 0 or 255 (255 means the pixel is in the range)
@@ -9,14 +12,19 @@ def classify():
     mask_orange = cv2.inRange(img_hsv, LOWER_ORANGE, UPPER_ORANGE)
     mask_green = cv2.inRange(img_hsv, LOWER_GREEN, UPPER_GREEN)
 
-    green_pixels = np.count_nonzero(mask_green)
     red_pixels = np.count_nonzero(mask_red)
     orange_pixels = np.count_nonzero(mask_orange)
+    green_pixels = np.count_nonzero(mask_green)
 
     if green_pixels > red_pixels and green_pixels > orange_pixels:
-        cv2.putText(img, "GO", (250, 250), 0, 1, (255, 255, 255), 4)
+        cv2.putText(img, "GO", (0, 250), 0, 10, (0, 255, 0), 10)
+        # Play success beep (1000Hz for 200ms)
+        winsound.Beep(1250, 200)
+        sleep(1)
     else:
-        cv2.putText(img, "STOP", (250, 250), 0, 1, (255, 255, 255), 4)
+        cv2.putText(img, "STOP", (0, 250), 0, 10, (0, 0, 255), 10)
+        # Play error beep (500Hz for 300ms)
+        winsound.Beep(500, 3000)
         
 def scan():
     try:
@@ -37,13 +45,12 @@ UPPER_ORANGE = np.array([88, 255, 255])
 LOWER_GREEN = np.array([69, 63, 97])
 UPPER_GREEN = np.array([135, 255, 255])  
 
-cap = cv2.VideoCapture(0)
+cap = WebcamVideoStream(src = 0).start()
 detector = cv2.QRCodeDetector()
 
 while True:
-    success, img = cap.read()
-    img = imutils.resize(img, width=400)
-    
+    img = cap.read()
+
     data, bbox = scan()
     
     if data != "":
